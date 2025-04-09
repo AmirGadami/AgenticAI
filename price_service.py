db9acd1dcd77f9e41e2609d08e1634f77e4d7ce3
AmirGadami <aghad051@uottawa.ca>
2025-04-09 13:38:04 -0400

    add the price-service class

--- price_service.py
+++ price_service.py
@@ -24,50 +24,6 @@ PREFIX = "Price is $"
 
 
 
-# @app.function(image=image, secrets=secrets, gpu=GPU, timeout=1800)
-# def price(description: str) -> float:
-#     import os
-#     import torch
-#     import re
-#     from transformers import AutoTokenizer, AutoModelForCausalLM,BitsAndBytesConfig, set_seed
-#     from peft import PeftModel
-
-
-#     QUESTION = "How much does this cost to the nearest dollar?"
-#     PREFIX = "Price is $"
-
-#     prompt = f"{QUESTION}\n{description}\n{PREFIX}"
-
-#     quant_config = BitsAndBytesConfig(
-#         load_in_4bit=True,
-#         bnb_4bit_use_double_quant=True,
-#         bnb_4bit_compute_dtype=torch.bfloat16,
-#         bnb_4bit_quant_type="nf4"
-#     )
-
-
-#     tokenizer = AutoTokenizer.from_pretrained(BASED_MODEL)
-#     tokenizer.pad_token = tokenizer.eos_token
-#     tokenizer.padding_side="right"
-
-#     base_model = AutoModelForCausalLM.from_pretrained(BASED_MODEL,
-#                                                       quantization_config = quant_config,
-#                                                       device_map='auto')
-
-#     fine_tuned_model = PeftModel.from_pretrained(base_model, FINETUNED_MODEL, revision = REVISION)
-
-#     set_seed(23)
-#     inputs = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
-#     attention_mask = torch.ones(inputs.shape,device='cuda')
-#     outputs = fine_tuned_model.generate(inputs,attention_mask=attention_mask, max_new_tokens=5, num_return_sequences=1)
-#     result = tokenizer.decode(outputs[0])
-
-#     contents = result.split("Price is $")[1]
-#     contents = contents.replace(',',"")
-#     match = re.search(r"[-+]?\d*\.\d+|\d+", contents)
-#     return float(match.group()) if match else 0.0
-
-
 
 
 
@@ -75,15 +31,15 @@ PREFIX = "Price is $"
 
 @app.cls(image=image, secrets=secrets, gpu =GPU, timeout=1800)
 class Pricer:
-    @modal.build(self)
+    @modal.build()
     def download_model_to_folder(self):
         from huggingface_hub import snapshot_download
         import os
         os.makedirs(MODEL_DIR, exist_ok=True)
-        snapshot_download(BASED_MODEL,local_dir=BASED_MODEL)
-        snapshot_download(FINETUNED_MODEL,local_dir=FINETUNED_MODEL)
+        snapshot_download(BASED_MODEL,local_dir=BASE_DIR)
+        snapshot_download(FINETUNED_MODEL,local_dir=FINETUNED_DIR)
 
-    @modal.enter
+    @modal.enter()
     def setup(self):
         import os
         import torch
@@ -132,3 +88,49 @@ class Pricer:
     @modal.method()
     def wake_up(self) -> str:
         return "ok"
+    
+
+
+# @app.function(image=image, secrets=secrets, gpu=GPU, timeout=1800)
+# def price(description: str) -> float:
+#     import os
+#     import torch
+#     import re
+#     from transformers import AutoTokenizer, AutoModelForCausalLM,BitsAndBytesConfig, set_seed
+#     from peft import PeftModel
+
+
+#     QUESTION = "How much does this cost to the nearest dollar?"
+#     PREFIX = "Price is $"
+
+#     prompt = f"{QUESTION}\n{description}\n{PREFIX}"
+
+#     quant_config = BitsAndBytesConfig(
+#         load_in_4bit=True,
+#         bnb_4bit_use_double_quant=True,
+#         bnb_4bit_compute_dtype=torch.bfloat16,
+#         bnb_4bit_quant_type="nf4"
+#     )
+
+
+#     tokenizer = AutoTokenizer.from_pretrained(BASED_MODEL)
+#     tokenizer.pad_token = tokenizer.eos_token
+#     tokenizer.padding_side="right"
+
+#     base_model = AutoModelForCausalLM.from_pretrained(BASED_MODEL,
+#                                                       quantization_config = quant_config,
+#                                                       device_map='auto')
+
+#     fine_tuned_model = PeftModel.from_pretrained(base_model, FINETUNED_MODEL, revision = REVISION)
+
+#     set_seed(23)
+#     inputs = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+#     attention_mask = torch.ones(inputs.shape,device='cuda')
+#     outputs = fine_tuned_model.generate(inputs,attention_mask=attention_mask, max_new_tokens=5, num_return_sequences=1)
+#     result = tokenizer.decode(outputs[0])
+
+#     contents = result.split("Price is $")[1]
+#     contents = contents.replace(',',"")
+#     match = re.search(r"[-+]?\d*\.\d+|\d+", contents)
+#     return float(match.group()) if match else 0.0
+
